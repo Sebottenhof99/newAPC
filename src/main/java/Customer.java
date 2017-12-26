1b2fc2d9e423d59fc5c2506d180c6e19349f54e5
Anton <anton.ponomarenko@gmx.de>
2017-12-26 20:39:12 +0100

    First Try to Read File. Create Customers

---
+++ src/main/java/Customer.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Antoshka on 26.12.2017.
+ */
+public class Customer {
+}
--- src/main/java/ooo.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by fisch on 26.12.2017.
- */
-public class ooo {
-}
