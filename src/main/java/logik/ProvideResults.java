bb7587ad8d5d48ce4a422a24efc2072e1c30a94d
Adam Fischbach <fischbachadam92@gmail.com>
2018-01-08 01:09:41 +0100

    provideResults

---
+++ src/main/java/logik/ProvideResults.java
@@ -0,0 +1,68 @@
+package logik;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.*;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.stream.Stream;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
+public class ProvideResults {
+
+    public static void moveAllFilesToTargetFolder() throws IOException {
+        Path root = Paths.get("C:\\Users\\"+System.getProperty("user.name")+"\\Desktop\\Ama_Rechnungen");
+        Path work = Paths.get("C:\\Users\\"+System.getProperty("user.name")+"\\Desktop\\Ama_Rechnungen\\work");
+        Date date = new Date();
+        SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy");
+        String year = yearFormat.format(date);
+        SimpleDateFormat monthFormat = new SimpleDateFormat("MMMM");
+        String month = monthFormat.format(date);
+        SimpleDateFormat dayFormat = new SimpleDateFormat("dd");
+        String day = dayFormat.format(date);
+
+
+        String wholePath = root.toString()+ File.separator + year + File.separator + month + File.separator +day+File.separator;
+        Path dist = Paths.get(wholePath);
+        // Files.createDirectories( Paths.get(wholePath));
+
+
+        try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(work)) {
+            for (Path path : directoryStream) {
+                Path d2 = dist.resolve(path.getFileName());
+                Files.move(path, d2, REPLACE_EXISTING);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+
+
+    }
+
+    public static String createFolder() throws IOException{
+        Path root = Paths.get("C:\\Users\\"+System.getProperty("user.name")+"\\Desktop\\Ama_Rechnungen");
+        Path work = Paths.get("C:\\Users\\"+System.getProperty("user.name")+"\\Desktop\\Ama_Rechnungen\\work");
+        Date date = new Date();
+        SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy");
+        String year = yearFormat.format(date);
+        SimpleDateFormat monthFormat = new SimpleDateFormat("MMMM");
+        String month = monthFormat.format(date);
+        SimpleDateFormat dayFormat = new SimpleDateFormat("dd");
+        String day = dayFormat.format(date);
+
+
+        String wholePath = root.toString()+ File.separator + year + File.separator + month + File.separator +day+File.separator;
+        Path dist = Paths.get(wholePath);
+        Files.createDirectories( Paths.get(wholePath));
+        return wholePath;
+    }
+
+
+}
