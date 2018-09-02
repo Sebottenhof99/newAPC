f923cdacc2b057081ecf595625661e457acdd60a
Adam Fischbach <fischbachadam92@gmail.com>
2018-09-02 18:49:18 +0200

    ddd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,11 @@
-package logik;
-
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import logik.CreateListOfCustomers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.List;
 
 /**
  * Created by Antoshka on 26.12.2017.
